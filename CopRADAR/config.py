3d493efd44b29facd1c2a33685157f988127bab0
cmsxkl <96667167+krasilovalex@users.noreply.github.com>
2025-09-18 20:01:38 +0300

    Update config.py

--- CopRADAR/config.py
+++ CopRADAR/config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN = ""
+BOT_TOKEN = "bot api token"
 CHANNEL_ID = 0
-HELP_CHAT_LINK = "link"
+HELP_CHAT_LINK = "link chat (https://t.me/example)"
+BOT_USER = "link bot without @"
+ADMINS_ID = 0
